15836ec32053d132d4a6b0ef7b4c4282401c9625
Steven Serrano <steven.serrano@tup.edu.ph>
2021-04-21 15:32:50 +0800

    ls.bat - show hidden files

--- ls.bat
+++ ls.bat
@@ -1,14 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
-for /D %%s in (.\*) do (
+for /f "tokens=*" %%s in ('DIR /A:D /b') do (
 	set line=%%s
-	echo [34m!line:~2,100!\[0m
+	echo [34m%%s\[0m
 )
 
 for %%f in (.\*) do (
 	set line=%%f
-	set exe_ext=!line:~-3,5!
-	if !exe_ext! equ exe (
+	set ext=!line:~-3,5!
+	set bool=false
+	if !ext! equ exe set bool=true
+	if !ext! equ bat set bool=true
+	if !ext! equ cmd set bool=true
+	if !bool! equ true (
 		echo [33m!line:~2,100![0m
 	) else (
 		echo !line:~2,100!
