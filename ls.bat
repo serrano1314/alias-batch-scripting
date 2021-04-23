b182aec076cfad561ed49d9b0391a27bd3021585
Steven Serrano <steven.serrano@tup.edu.ph>
2021-04-23 13:54:55 +0800

    modify code using only 1 for loop

--- ls.bat
+++ ls.bat
@@ -1,22 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
-for /f "tokens=*" %%s in ('DIR %~1 /A:D /b') do (
-	set line=%%s
-	echo  [46m [0m [34m%%s\[0m
-)
-for /f "tokens=*" %%f in ('DIR %~1 /A:A-D /b') do (
+::for /f "tokens=*" %%s in ('DIR %~1 /A:D /b') do (
+::	set line=%%s
+::	echo [46m [0m [34m%%s\[0m
+::)
+for /f "tokens=*" %%f in ('DIR %~1 /A /b') do (
 	set line=%%f
 	set ext=!line:~-3,5!
-	set bool=false
-	if !ext! equ exe set bool=true
-	if !ext! equ .py set bool=true
-	if !ext! equ bat set bool=true
-	if !ext! equ cmd set bool=true
-	if !ext! equ com set bool=true
-	if !bool! equ true (
-		echo  [43m [0m [33m!line:~0,100![0m
+	IF EXIST %%f\NUL (
+		ECHO [46m [0m [34m%%f\[0m
 	) else (
-		echo  [47m [0m !line:~0,100!
+		set bool=false
+		if !ext! equ exe set bool=true
+		if !ext! equ .py set bool=true
+		if !ext! equ bat set bool=true
+		if !ext! equ cmd set bool=true
+		if !ext! equ com set bool=true
+		if !bool! equ true (
+			echo [43m [0m [33m!line![0m
+		) else (
+			echo [47m [0m !line!
+		)
 	)
 )
 endlocal
