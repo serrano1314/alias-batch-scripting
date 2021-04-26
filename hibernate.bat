734f907248e6c634adfeab67e67113fb297ba334
Steven Serrano <steven.serrano@tup.edu.ph>
2021-04-27 01:38:48 +0800

    fix the path for hiber log

--- hibernate.bat
+++ hibernate.bat
@@ -1,6 +1,7 @@
 @echo off
 echo.
 echo Hibernate:  %date% - %time:~0,5%
-echo %date% - %time:~0,5% >> hiber_log.txt
+echo HIBERNATE: %date% - %time:~0,5% >> C:\alias-batch-scripting\hiber_log.txt
 echo.
 shutdown /h /f 
+echo UP TO: %date% - %time:~0,5% >> C:\alias-batch-scripting\hiber_log.txt
