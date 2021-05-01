d243a0522b2ef17f214a5259339b49ef4ca0d47e
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-01 14:57:52 +0800

    add some changes

--- cmd_title.bat
+++ cmd_title.bat
@@ -4,5 +4,5 @@ for %%I in (.) do (
 	set CurrDirName=%%~nxI
 )
 title ⚡ %CurrDirName%
-prompt $E[1;33;40m$d$s$b$t$h$h$h$s$s$_$E[1;96;40m%CurrPath%$E[1;95;4m%CurrDirName%[0m$E[1;91;40m$_$$~[97m
+prompt $E[1;33;40m$d$s$b$t$h$h$h$s$s$_$E[1;96;40m%CurrPath%$h$E[1;95;40m\$E[1;95;4m%CurrDirName%[0m$_$E[1;91;40m$$~[97m
 
--- ls.bat
+++ ls.bat
@@ -4,7 +4,7 @@ for /f "tokens=*" %%f in ('DIR %~1 /A /b') do (
 	set line=%%f
 	set ext=!line:~-3,5!
 	if exist %%f\nul (
-		echo [46m [0m [34m%%f\[0m
+		echo [46m [0m [94m%%f\[0m
 	) else (
 		set bool=false
 		if !ext! equ exe set bool=true
@@ -14,7 +14,7 @@ for /f "tokens=*" %%f in ('DIR %~1 /A /b') do (
 		if !bool! equ true (
 			echo [43m [0m [33m!line![0m
 		) else (
-			echo [47m [0m !line!
+			echo [47m [0m [97m!line![0m
 		)
 	)
 )
