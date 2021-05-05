4d06ebdc6a78d80e85d286dc719fd8f165331436
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-05 20:31:43 +0800

    add icon for some different file extensions

--- cmd_title.bat
+++ cmd_title.bat
@@ -6,12 +6,12 @@ for %%I in (.) do (
 )
 if exist .git (
 	set onGit= $h 
-	@git rev-parse --abbrev-ref HEAD> branchtemp
-	for /f "delims=" %%i in (branchtemp) do set branch= %%i
+	@git rev-parse --abbrev-ref HEAD> %temp%\branchtemp
+	for /f "delims=" %%i in (%temp%\branchtemp) do set branch= %%i
 ) else (
 	set onGit=
 	set branch=
 )
-if exist branchtemp del branchtemp
+if exist branchtemp del %temp%\branchtemp
 title ⚡ %CurrDirName%
-prompt $E[1;33;40m  $d$s$b$t$h$h$h$s$s$_[30m$E[1;94;100m$E[1;0;100m %CurrPath%$h$E[1;30;47m $E[1;95;47m%CurrDirName%[34m%onGit%[92m%branch%[;97;0m[0m$_$E[1;91;40m [97m
+prompt $E[1;33;40m  $d$s$b$t$h$h$h$s$s$_[30m$E[1;94;100m$E[1;0;100m %CurrPath%$h$E[1;30;47m $E[1;95;47m %CurrDirName%[34m%onGit%[92m%branch%[;97;0m[0m$_$E[1;91;40m [97m
--- ls.bat
+++ ls.bat
@@ -1,10 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
+set file_icon=
 for /f "tokens=*" %%f in ('DIR %~1 /A /b') do (
 	set line=%%f
 	set ext=!line:~-3,5!
+	set ext2=!line:~-2,5!
+	set ext3=!line:~-4,5!
 	if exist %%f\nul (
-		echo [94m %%f\[0m
+		if exist %%f\.git (
+			echo [94m %%f\[0m
+		) else (
+			echo [94m %%f\[0m
+		)
 	) else (
 		set boolx=false
 		if !ext! equ exe set boolx=true
@@ -12,9 +19,26 @@ for /f "tokens=*" %%f in ('DIR %~1 /A /b') do (
 		if !ext! equ cmd set boolx=true
 		if !ext! equ com set boolx=true
 		if !boolx! equ true (
-			echo [33m﩯!line![0m
+			echo [33m !line![0m
 		) else (
-			echo [97m !line![0m
+			if !ext! equ mp4 set file_icon=
+			if !ext! equ mp3 set file_icon=
+			if !ext3! equ flac set file_icon=
+			if !ext! equ jpg set file_icon=
+			if !ext! equ peg set file_icon=
+			if !ext! equ .js set file_icon=
+			if !ext! equ tml set file_icon=
+			if !ext! equ css set file_icon=
+			if !ext! equ psd set file_icon=
+			if !ext! equ .ai set file_icon=
+			if !ext! equ .py set file_icon=
+			if !ext2! equ .c set file_icon=
+			if !ext! equ cpp set file_icon=
+			if !ext! equ pdf set file_icon=
+			if !ext3! equ docx set file_icon=
+			if !ext3! equ pptx set file_icon=
+			if !ext3! equ xlsx set file_icon=
+			echo [97m!file_icon! !line![0m
 		)
 	)
 )
