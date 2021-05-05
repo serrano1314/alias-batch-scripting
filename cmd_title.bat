a0423828c09d55a8ebb364aa3eacefb176962c0d
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-05 16:04:56 +0800

    add git logo when inside a local git repo folder

--- cmd_title.bat
+++ cmd_title.bat
@@ -2,7 +2,7 @@
 for %%I in (.) do (
 	set CurrPath=%%~dpI
 	set CurrDirName=%%~nxI
+	if exist .git set onGit=  
 )
 title ⚡ %CurrDirName%
-prompt $E[1;33;40m$d$s$b$t$h$h$h$s$s$_$E[1;96;40m%CurrPath%$h$E[1;95;40m\$E[1;95;4m%CurrDirName%[0m$_$E[1;91;40m$$~[97m
-
+prompt $E[1;33;40m$d$s$b$t$h$h$h$s$s$_[97m$E[1;94;107m%CurrPath%$h[0m[97m[0m[97m$E[1;91;107m%CurrDirName%%onGit%[0m[97m[0m$_$E[1;91;40m [97m
