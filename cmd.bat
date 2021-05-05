d6be1b7ac37285059fc931d6fab3e4f2da99a2a2
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-05 19:11:34 +0800

    add branch name for the prompt

--- cmd.bat
+++ cmd.bat
@@ -1,4 +1,6 @@
 @echo off
 doskey cd = cd /d $* ^&^& cmd_title
+doskey git = git $* ^&^& cmd_title
+
 doskey pwd = cd
 cmd_title
--- cmd_title.bat
+++ cmd_title.bat
@@ -1,3 +1,4 @@
+::must have droidsans nerd font
 @echo off
 for %%I in (.) do (
 	set CurrPath=%%~dpI
@@ -5,8 +6,12 @@ for %%I in (.) do (
 )
 if exist .git (
 	set onGit= $h 
+	@git rev-parse --abbrev-ref HEAD> branchtemp
+	for /f "delims=" %%i in (branchtemp) do set branch= %%i
 ) else (
 	set onGit=
+	set branch=
 )
+if exist branchtemp del branchtemp
 title ⚡ %CurrDirName%
-prompt $E[1;33;40m  $d$s$b$t$h$h$h$s$s$_[30m$E[1;94;100m$E[1;0;100m %CurrPath%$h$E[1;30;47m $E[1;95;47m%CurrDirName%[34m%onGit%[;97;0m[0m$_$E[1;91;40m [97m
+prompt $E[1;33;40m  $d$s$b$t$h$h$h$s$s$_[30m$E[1;94;100m$E[1;0;100m %CurrPath%$h$E[1;30;47m $E[1;95;47m%CurrDirName%[34m%onGit%[92m%branch%[;97;0m[0m$_$E[1;91;40m [97m
