be786d137ff04e28af117d6f4c609e7bdedd851f
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-05 17:51:47 +0800

    add design

--- cmd_title.bat
+++ cmd_title.bat
@@ -1,12 +1,12 @@
 @echo off
 for %%I in (.) do (
 	set CurrPath=%%~dpI
-	set CurrDirName=%%~nxI
+	set CurrDirName= %%~nxI 
 )
 if exist .git (
-	set onGit=  
+	set onGit= $h 
 ) else (
 	set onGit=
 )
 title ⚡ %CurrDirName%
-prompt $E[1;33;40m $d$s$b$t$h$h$h$s$s$_[97m$E[1;94;107m%CurrPath%$h[0m[97m[0m[97m$E[1;91;107m%CurrDirName%[95m%onGit%[0m[97m[0m$_$E[1;91;40m [97m
+prompt $E[1;33;40m  $d$s$b$t$h$h$h$s$s$_[30m$E[1;94;100m$E[1;0;100m %CurrPath%$h$E[1;30;47m $E[1;95;47m%CurrDirName%[34m%onGit%[;97;0m[0m$_$E[1;91;40m [97m
--- ls.bat
+++ ls.bat
@@ -4,17 +4,17 @@ for /f "tokens=*" %%f in ('DIR %~1 /A /b') do (
 	set line=%%f
 	set ext=!line:~-3,5!
 	if exist %%f\nul (
-		echo [46m [0m [94m%%f\[0m
+		echo [94m %%f\[0m
 	) else (
-		set bool=false
-		if !ext! equ exe set bool=true
-		if !ext! equ bat set bool=true
-		if !ext! equ cmd set bool=true
-		if !ext! equ com set bool=true
-		if !bool! equ true (
-			echo [43m [0m [33m!line![0m
+		set boolx=false
+		if !ext! equ exe set boolx=true
+		if !ext! equ bat set boolx=true
+		if !ext! equ cmd set boolx=true
+		if !ext! equ com set boolx=true
+		if !boolx! equ true (
+			echo [33m﩯!line![0m
 		) else (
-			echo [47m [0m [97m!line![0m
+			echo [97m !line![0m
 		)
 	)
 )
