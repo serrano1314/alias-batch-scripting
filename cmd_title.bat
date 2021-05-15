773f1f154cf751196a48bcf46e4a7b7f1d9c8cbe
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-15 22:51:11 +0800

    edit the comment

--- cmd_title.bat
+++ cmd_title.bat
@@ -1,11 +1,11 @@
-::must have droidsans nerd font
+::must have any nerd font
 @echo off
 for %%I in (.) do (
 	set CurrPath=%%~dpI
-	set CurrDirName= %%~nxI 
+	set CurrDirName= %%~nxI
 )
 if exist .git (
-	set onGit= $h 
+	set onGit= $h
 	@git rev-parse --abbrev-ref HEAD> %temp%\branchtemp
 	for /f "delims=" %%i in (%temp%\branchtemp) do set branch= %%i
 ) else (
@@ -17,9 +17,9 @@ title ⚡ %CurrDirName%
 netsh interface show interface | findstr /C:"Wi-Fi" > %temp%/tempWifiStatus
 for /f "delims=" %%I in (%temp%/tempWifiStatus) do set wifiStatus=%%I
 set wifiStatus=%wifiStatus:~15,10%
-if %wifiStatus% equ Connected ( 
+if %wifiStatus% equ Connected (
 	set wifiStatus=[0;92;100m直[0m
-) else ( 
+) else (
 	set wifiStatus=[0;91;100m睊[0m
 )
 prompt $E[1;33;44m  %USERNAME% $E[0;34;40m$E[1;30;40m%wifiStatus%$E[0;96;100m $d  $t$E[1;30;40m$h$h$h$_$E[1;30;40m$E[1;94;100m  $E[0;0;100m %CurrPath%$h$E[1;30;47m $E[0;95;47m%CurrDirName%[34m%onGit%[32m%branch%$E[0;47;40m[0m$_$E[0;91;40m[0;97;0m
