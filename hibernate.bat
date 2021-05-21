39b44872610ea5b8a7cc357278aa602c94c71bb9
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-21 12:16:06 +0800

    update the filename path

--- hibernate.bat
+++ hibernate.bat
@@ -1,7 +1,7 @@
 @echo off
 echo.
 echo Hibernate:  %date% - %time:~0,5%
-echo HIBERNATE: %date% - %time:~0,5% >> C:\alias-batch-scripting\hiber_log.txt
+echo HIBERNATE: %date% - %time:~0,5% >> C:\alias-batch-scripting\hiber.log
 echo.
 shutdown /h /f 
-echo UP TO: %date% - %time:~0,5% >> C:\alias-batch-scripting\hiber_log.txt
+echo UP TO: %date% - %time:~0,5% >> C:\alias-batch-scripting\hiber.log
