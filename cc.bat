055829c464536526012bcbfba8c5b93ef3fe0c83
Steven Serrano <steven.serrano@tup.edu.ph>
2021-07-30 23:19:31 +0800

    change equal to dash in display

--- cc.bat
+++ cc.bat
@@ -1,13 +1,13 @@
 @echo off
 echo [92mCalculator
-echo [91mx = exit  [93mc = clear [0m
+echo [91mx - exit  [93mc - clear [0m
 :loop
 set /p res= 
 if %res% equ x goto end
 if %res% equ c (
 	cls
 	echo [92mCalculator
-	echo [91mx = exit  [93mc = clear [0m
+	echo [91mx - exit  [93mc - clear [0m
 	)
 >"%temp%\VBS.vbs" echo Set fso = CreateObject("Scripting.FileSystemObject") : Wscript.echo (%res%)
 for /f "delims=" %%a in ('cscript /nologo "%temp%\VBS.vbs"') do set "res=%%a"
