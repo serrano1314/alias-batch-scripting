3fba96b94c3eb47eae40e7eb6eb86fe2ad13be70
Steven Serrano <steven.serrano@tup.edu.ph>
2021-07-30 23:17:03 +0800

    fix the display when clear pressed

--- cc.bat
+++ cc.bat
@@ -6,8 +6,8 @@ set /p res=
 if %res% equ x goto end
 if %res% equ c (
 	cls
-	echo x = exit
-	echo c = clear
+	echo [92mCalculator
+	echo [91mx = exit  [93mc = clear [0m
 	)
 >"%temp%\VBS.vbs" echo Set fso = CreateObject("Scripting.FileSystemObject") : Wscript.echo (%res%)
 for /f "delims=" %%a in ('cscript /nologo "%temp%\VBS.vbs"') do set "res=%%a"
