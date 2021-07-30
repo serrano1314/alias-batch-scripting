028dd48d169ce3b8769c827fb83f79ff52c4e49b
Steven Serrano <steven.serrano@tup.edu.ph>
2021-07-30 23:09:52 +0800

    add title and credit when exit

--- cc.bat
+++ cc.bat
@@ -1,17 +1,21 @@
 @echo off
+echo Calculator
 echo x = exit
 echo c = clear
 :loop
 set /p res=
->"%temp%\VBS.vbs" echo Set fso = CreateObject("Scripting.FileSystemObject") : Wscript.echo (%res%)
-for /f "delims=" %%a in ('cscript /nologo "%temp%\VBS.vbs"') do set "res=%%a"
-del "%temp%\VBS.vbs"
-echo [92m%res% [0m
 if %res% equ x goto end
 if %res% equ c (
 	cls
 	echo x = exit
 	echo c = clear
 	)
+>"%temp%\VBS.vbs" echo Set fso = CreateObject("Scripting.FileSystemObject") : Wscript.echo (%res%)
+for /f "delims=" %%a in ('cscript /nologo "%temp%\VBS.vbs"') do set "res=%%a"
+del "%temp%\VBS.vbs"
+echo [92m%res% [0m
+
 goto loop
 :end
+echo QUICK_CALC by stvnsrrn
+echo [91mhttps://www.github.com/serrano1314
